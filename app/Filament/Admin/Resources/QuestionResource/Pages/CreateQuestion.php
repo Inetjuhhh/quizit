33d1bcfe100aa4a8e65ae506ef61975effb9dc36
Inetjuhhh <35923627+Inetjuhhh@users.noreply.github.com>
2024-09-08 15:48:25 +0200

    add created_by after fill

--- app/Filament/Admin/Resources/QuestionResource/Pages/CreateQuestion.php
+++ app/Filament/Admin/Resources/QuestionResource/Pages/CreateQuestion.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateQuestion extends CreateRecord
 {
     protected static string $resource = QuestionResource::class;
+
+    protected function afterFill() : void
+    {
+        $user = auth()->user();
+        $this->data['created_by'] = $user->id;
+    }
+
 }
