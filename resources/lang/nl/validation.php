10fe66582d1aab87e1a2c13d86da4f0bf4515f3a
Inetjuhhh <35923627+Inetjuhhh@users.noreply.github.com>
2024-09-08 15:46:33 +0200

    change language to nl

---
+++ resources/lang/nl/validation.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'answer'                => 'antwoord',
+    'answers'               => 'antwoorden',
+    'category'              => 'categorie',
+    'create'                => 'aanmaken',
+    'edit'                  => 'bewerken',
+]
+
+?>
