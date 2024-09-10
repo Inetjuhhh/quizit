413d61c9aac247e9d1554d2d2dbeb918eeae6e37
Inetjuhhh <35923627+Inetjuhhh@users.noreply.github.com>
2024-09-10 16:26:54 +0200

    Fixes #6 - Add question to new and edited answer

--- app/Filament/Admin/Resources/QuestionResource/RelationManagers/AnswersRelationManager.php
+++ app/Filament/Admin/Resources/QuestionResource/RelationManagers/AnswersRelationManager.php
@@ -3,7 +3,11 @@
 namespace App\Filament\Admin\Resources\QuestionResource\RelationManagers;
 
 use Filament\Forms;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ViewField;
 use Filament\Forms\Form;
+use Filament\Resources\Pages\ViewRecord;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -12,15 +16,31 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class AnswersRelationManager extends RelationManager
 {
+    protected static ?string $model = \App\Models\Answer::class;
+
     protected static string $relationship = 'answers';
+
+    protected static ?string $navigationLabel = 'Antwoorden';
+
+    protected static ?string $modelLabel = 'Antwoord';
+
+    protected static ?string $pluralModelLabel = 'Antwoorden';
 
     public function form(Form $form): Form
     {
         return $form
             ->schema([
-                
+                TextInput::make('question')
+                ->formatStateUsing(function () {
+                    return $this->ownerRecord->question;
+                })
+                    ->default($this->ownerRecord->question)
+                    ->dehydrated()
+                    ->disabled()
+                    ->label('Vraag'),
                 Forms\Components\TextInput::make('answer')
                     ->required()
+                    ->label('Antwoord')
                     ->maxLength(255),
                 Forms\Components\Toggle::make('is_correct')
                     ->label('Is juist?')
