15d6860224ae3e75aadd58d3c2caa43c5803b465
Inetjuhhh <35923627+Inetjuhhh@users.noreply.github.com>
2024-09-24 15:59:01 +0200

    small fix in header

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,7 +2,7 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex justify-between w-full">
+            <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <a href="{{ route('dashboard') }}">
