7200608e0a1b67f6840bc23fd68699e2a37abd6f
Inetjuhhh <35923627+Inetjuhhh@users.noreply.github.com>
2024-10-18 20:55:36 +0200

    delete comments

--- app/View/Components/ButtonScore.php
+++ app/View/Components/ButtonScore.php
@@ -8,9 +8,6 @@ use Illuminate\View\Component;
 
 class ButtonScore extends Component
 {
-    /**
-     * Create a new component instance.
-     */
     public $answerText;
     public $isCorrect;
     public $buttonText;
@@ -22,7 +19,6 @@ class ButtonScore extends Component
         $this->buttonText = $buttonText;
     }
 
-    // Method to determine the button class based on correctness
     public function getButtonClass()
     {
         if ($this->isCorrect === 1) {
@@ -30,13 +26,10 @@ class ButtonScore extends Component
         } elseif ($this->isCorrect === 0) {
             return 'focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900';
         } else {
-            return 'focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900';
+            return 'focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900';
         }
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.button-score');
