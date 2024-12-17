49b095a156bd067908e0c135dc6799db37bd1a46
Ine <ine@inetjuhhh.nl>
2024-12-17 20:12:08 +0100

    styling of fillable livewire response input field

--- resources/views/livewire/quiz-attempt/response-open-question-score.blade.php
+++ resources/views/livewire/quiz-attempt/response-open-question-score.blade.php
@@ -1,4 +1,4 @@
 <div class="flex flex-col justify-center">
-    <input type="text" wire:change="setScore" wire:model="score" class="@if($score > 0)bg-lime-700 text-white text-center @endif w-12 rounded-lg bg-transparent">
+    <input type="text" wire:change="setScore" wire:model="score" class="@if($score > 0)bg-lime-700 text-white  @endif w-12 rounded-lg bg-transparent text-center">
     <label class="italic" for="">In te vullen door de docent</label>
 </div>
