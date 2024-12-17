e9cbef49baa62f0b3f1edaef0b84554a89b10228
Ine <ine@inetjuhhh.nl>
2024-12-17 19:40:39 +0100

    a bit of styling for score component of userQuizResponse page

--- resources/views/livewire/quiz-attempt/response-open-question-score.blade.php
+++ resources/views/livewire/quiz-attempt/response-open-question-score.blade.php
@@ -1,3 +1,4 @@
-<div>
-    <input type="text" wire:change="setScore" wire:model="score" class="@if($score > 0)bg-orange-500 text-white text-center @endif w-12 rounded-lg bg-transparent">
+<div class="flex flex-col justify-center">
+    <input type="text" wire:change="setScore" wire:model="score" class="@if($score > 0)bg-lime-700 text-white text-center @endif w-12 rounded-lg bg-transparent">
+    <label class="italic" for="">In te vullen door de docent</label>
 </div>
