77be5f6283865aabfccdf000753a251fbab13a45
Ine <ine@inetjuhhh.nl>
2024-12-23 22:00:19 +0100

    give notice that quiz should be reviewed by teacher

--- resources/views/userquiz/index.blade.php
+++ resources/views/userquiz/index.blade.php
@@ -20,7 +20,10 @@
                 <tbody>
                     @foreach ($executedUserQuizAttempts as $executedUserQuizAttempt)
                         <tr class="border-b border-gray-200 dark:border-gray-700 text-xl">
-                            <td scope="row" class="hover:bg-gray-900 px-6 py-3 text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800 border-r"><a href="{{ route('userquiz.result', ['id' => $executedUserQuizAttempt->id])}}" class="text-slate-500" >{{$executedUserQuizAttempt->attempt->quiz->name}}</a></td>
+                            <td scope="row" class="hover:bg-gray-900 px-6 py-3 text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800 border-r">
+                                <a href="{{ route('userquiz.result', ['id' => $executedUserQuizAttempt->id])}}" class="text-slate-500" >{{$executedUserQuizAttempt->attempt->quiz->name}}</a>
+                                <p class="italic text-sm text-red-400">@if($executedUserQuizAttempt->responses->contains('is_correct', null))(Dit resultaat moet nog beoordeeld worden door een docent.)@endif</p>
+                            </td>
                             <td scope="row" class="px-6 py-3 text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800 border-r">{{$executedUserQuizAttempt->responses->sum('is_correct')}} / {{$numberOfQuestions}} </td>
                             <td scope="row" class="px-6 py-3 text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">{{\Carbon\Carbon::parse($executedUserQuizAttempt->completed_at)->format('d-m-Y H:i')}}</td>
                         </tr>
