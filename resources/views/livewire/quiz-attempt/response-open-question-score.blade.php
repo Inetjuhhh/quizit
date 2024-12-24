9bac61485b221976b135a6487eb669d018ac3ae4
Ine <ine@inetjuhhh.nl>
2024-12-24 09:45:00 +0100

    give user response styling

--- resources/views/livewire/quiz-attempt/response-open-question-score.blade.php
+++ resources/views/livewire/quiz-attempt/response-open-question-score.blade.php
@@ -1,4 +1,9 @@
+
 <div class="flex flex-col justify-center">
-    <input type="text" wire:change="setScore" wire:model="score" class="@if($score > 0)bg-lime-700 text-white  @endif w-12 rounded-lg bg-transparent text-center">
-    <label class="italic" for="">In te vullen door de docent</label>
+    <input type="text" wire:change="setScore" wire:model="score" class="text-white w-12 rounded-lg bg-transparent text-center @if($score > 0)bg-green-800 @elseif($score == 0) bg-red-900 @endif">
+    @if($score !== NULL)
+        <label class="italic" for="">Score ingevuld</label>
+    @else
+        <label class="italic" for="">Geef een score in</label>
+    @endif
 </div>
